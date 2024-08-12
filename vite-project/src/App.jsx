64ef9425dd365a39cb766d144e82477e0acea9ff
Mathie <math039@outlook.fr>
2024-08-12 22:08:00 +0200

    input

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -6,23 +6,25 @@ import { InputText } from "./components/Textinputt";
 import { ProductTow } from "./components/form/ProductRow";
 
 import { ProductsCate } from "./components/form/ProductsCate";
+import { CateAndStock } from "./components/form/CateAndStock";
 import { Fragment } from "react";
 
 const PRODUCTS = [
   { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
-  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Fruits", price: "$6", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$5", stocked: false, name: "Passionfruit" },
   { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
   { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
-  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+  { category: "Vegetables", price: "$12", stocked: true, name: "Peas" },
 ];
 
 function App() {
   const [stocked, SetStocked] = useState(false);
   return (
     <Fragment>
-      <SearBar siIlestStock={stocked} onstockr />
+      <SearBar showStockedOnly={stocked} onststocked={SetStocked} />
       <ProductTable Products={PRODUCTS} />
+      <CateAndStock product={PRODUCTS} />
     </Fragment>
   );
 }
@@ -55,33 +57,29 @@ function ProductTable({ Products }) {
     </table>
   );
 }
-function SearBar() {
+function SearBar(showStockedOnly, onststocked) {
   const [Check, SetCheck] = useState(false);
 
   const [value, setValue] = useState("");
-  function handleChange(e) {
-    setValue(e);
-    console.log(value);
-  }
-  function ChangeCheck() {
-    for (let i = 0; i < PRODUCTS.length; i++) {
-      if (PRODUCTS.stocked === true) {
-        console.log(PRODUCTS);
-      }
-    }
-    SetCheck(!Check);
-  }
+  // function handleChange(e) {
+  //   setValue(e);
+  //   console.log(value);
+  // }
+  // function ChangeCheck() {
+  //   for (let i = 0; i < PRODUCTS.length; i++) {
+  //     if (PRODUCTS.stocked === true) {
+  //       console.log(PRODUCTS);
+  //     }
+  //   }
+  //   SetCheck(!Check);
+  // }
 
   return (
     <div style={{ marginBottom: "20px" }}>
-      <InputText
-        value={value}
-        onChange={handleChange}
-        placeholder="Rechercher"
-      />
+      <InputText value={value} placeholder="Rechercher" />
       <Checkbox
-        checked={Check}
-        onChange={ChangeCheck}
+        checked={showStockedOnly}
+        onChange={onststocked}
         id={"stocked"}
         label={"n'afficher que les produits en stock"}
       />
---
+++ vite-project/src/components/form/CateAndStock.jsx
@@ -0,0 +1,29 @@
+export function CateAndStock({ product }) {
+  let EnStock;
+  for (let i = 0; i < product.length; i++) {
+    const style = product[i].stocked ? (
+      <li>Il en reste</li>
+    ) : (
+      <li>Il y en a plus en stock</li>
+    );
+
+    console.log(product[i].category);
+
+    if (product[i].stocked === true) {
+      EnStock = "Il en reste";
+    } else {
+      EnStock = "Il y en a plus en stock";
+    }
+    let number = 0;
+
+    return (
+      <>
+        {product.map((e) => (
+          <ul style={{ listStyle: "none", display: "flex" }}>
+            <li key={e.name}> {e.name}</li>,<li key={e.price}> {style} </li>
+          </ul>
+        ))}
+      </>
+    );
+  }
+}
